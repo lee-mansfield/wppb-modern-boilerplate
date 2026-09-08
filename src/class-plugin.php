f92ff336964a54a052c7ae32a9be09c871353ac5
Lee Mansfield <leemansfield@hotmail.com>
2026-09-08 10:31:37 +0100

    Applied minor change to constant names

--- src/class-plugin.php
+++ src/class-plugin.php
@@ -22,12 +22,12 @@ use Modern\Plugin\Settings\SettingsPage;
 require dirname( __DIR__, 1 ) . '/plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
-$set_repo_url = defined( 'MAXIMUM_OVERDRIVE_PLUGIN_GIT_URL' )
-				? constant( 'MAXIMUM_OVERDRIVE_PLUGIN_GIT_URL' )
+$set_repo_url = defined( 'WPPB_CI_CD_PLUGIN_GIT_URL' )
+				? constant( 'WPPB_CI_CD_PLUGIN_GIT_URL' )
 				: '';
 
-$set_plugin_slug = defined( 'MAXIMUM_OVERDRIVE_PLUGIN_SLUG' )
-					? constant( 'MAXIMUM_OVERDRIVE_PLUGIN_SLUG' )
+$set_plugin_slug = defined( 'WPPB_CI_CD_PLUGIN_SLUG' )
+					? constant( 'WPPB_CI_CD_PLUGIN_SLUG' )
 					: '';
 
 if (
@@ -44,8 +44,8 @@ if (
 	$mo_plugin_update_checker->setBranch( 'main' );
 
 	// Optional: If you're using a private repository, specify the access token like this.
-	$set_token = defined( 'MAXIMUM_OVERDRIVE_PLUGIN_RELEASE_TOKEN' )
-					? constant( 'MAXIMUM_OVERDRIVE_PLUGIN_RELEASE_TOKEN' )
+	$set_token = defined( 'WPPB_CI_CD_PLUGIN_GIT_RELEASE_TOKEN' )
+					? constant( 'WPPB_CI_CD_PLUGIN_GIT_RELEASE_TOKEN' )
 					: '';
 	$mo_plugin_update_checker->setAuthentication( $set_token );
 }
